5594592cebbf8f82a2a6536eb27fe67410b1ec36
byronPantoja <byronpantoja@gmail.com>
2020-04-11 13:30:09 +0800

    Refactoring autocomplete rendering function

--- autocomplete.js
+++ autocomplete.js
@@ -1,5 +1,6 @@
 const createAutoComplete = ({
-    root
+    root,
+    renderOption
 }) => {
     root.innerHTML = `
       <label><b>Search For a Movie</b></label>
@@ -27,13 +28,9 @@ const createAutoComplete = ({
         dropdown.classList.add('is-active');
         for (let movie of movies) {
             const option = document.createElement('a');
-            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
             option.classList.add('dropdown-item');
-            option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-      `;
+            option.innerHTML = renderOption(movie);
             option.addEventListener('click', () => {
                 dropdown.classList.remove('is-active');
                 input.value = movie.Title;
